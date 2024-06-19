727d021a142c96f26f213644cbaddb47e03e0180
Lyton <65817176+Lyton505@users.noreply.github.com>
2024-06-19 22:42:24 +0000

    database-setup initial commit

---
+++ lib/Auth.ts
@@ -0,0 +1,37 @@
+import { supabase } from "./supabase" 
+
+/**
+ * Signs up the user with an email and password
+ * @param {string} email - the email to sign up with
+ * @param {string} password - the password to signup with
+ * 
+ */
+export async function signUpWithEmail(email: string, password: string) {
+    const {
+        data: { session },
+        error,
+    } = await supabase.auth.signUp({
+        email: email,
+        password: password,
+    })
+
+    // TODO: If error then return error code
+    if (error) console.log("Error signing up with email");
+    else {console.log("Success signing in with email")};
+}
+
+/**
+ * Signs in the user with a valid email and password
+ * @param {string} email - the email to sign in with
+ * @param {string} password - the password to sign in with
+ */
+export async function signInWithEmail(email: string, password: string) {
+    const { error } = await supabase.auth.signInWithPassword({
+      email: email,
+      password: password,
+    })
+
+    // TODO: if error then return error code
+    if (error) console.log("Error signing up with email");
+    else {console.log("Success signing in with email")};
+  }
